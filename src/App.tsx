a807bd80d500f2474efa49af78e96d8ee411ef76
Daniel Kral <dakral@protonmail.com>
2024-01-19 19:13:54 +0100

    Change color scheme and add dark mode; Adapt global style; Add font files and change fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './assets/base.css';
 import './assets/app.css';
+import './assets/assets.css';
+
 import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
 
 const App = () => {
-	return (
-		<RouterProvider router={router} />
-	);
+	return <RouterProvider router={router} />;
 };
 
 export default App;
