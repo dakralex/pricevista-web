2f4aac6ebbbe253ccd9f1c89d43f1c6947698e34
Daniel Kral <dakral@protonmail.com>
2024-01-20 19:23:15 +0100

    Change background color and improve neumorphism in cards

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -2,13 +2,13 @@ import './Sidebar.css';
 import SearchFilterBar from './SearchFilterBar.tsx';
 
 interface SideBarProps {
-	isHidden?: boolean;
+  isHidden?: boolean;
 }
 
-const Sidebar = ({ isHidden }: SideBarProps) => {
-	return <div className={`pv-sidebar ${isHidden && 'hidden'}`}>
-		<SearchFilterBar />
-	</div>;
+const Sidebar = ({isHidden}: SideBarProps) => {
+  return <div className={`pv-sidebar ${isHidden && 'hidden'}`}>
+    <SearchFilterBar />
+  </div>;
 };
 
 export default Sidebar;
