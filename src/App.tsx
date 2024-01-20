6c6a3d607f7158d01f376d186784fccb5f74f3ed
Daniel Kral <dakral@protonmail.com>
2024-01-20 19:16:07 +0100

    Fix indentation in index files

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import './assets/base.css';
 import './assets/app.css';
 import './assets/assets.css';
 
-import { RouterProvider } from 'react-router-dom';
+import {RouterProvider} from 'react-router-dom';
 import router from './router.tsx';
 
 const App = () => {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/router.tsx
+++ src/router.tsx
@@ -1,29 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom';
+import {createBrowserRouter} from 'react-router-dom';
 import Root from './routes/Root.tsx';
 import HomePage from './routes/pages/HomePage.tsx';
 import SearchPage from './routes/pages/SearchPage.tsx';
 import ArticlePage from './routes/pages/ArticlePage.tsx';
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Root />,
-		// TODO Add custom errorElement later on
-		children: [
-			{
-				path: '/',
-				element: <HomePage />
-			},
-			{
-				path: '/search',
-				element: <SearchPage />
-			},
-			{
-				path: '/articles/:articleId',
-				element: <ArticlePage />
-			}
-		]
-	}
+  {
+    path: '/',
+    element: <Root />,
+    // TODO Add custom errorElement later on
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/search',
+        element: <SearchPage />,
+      },
+      {
+        path: '/articles/:articleId',
+        element: <ArticlePage />,
+      },
+    ],
+  },
 ]);
 
 export default router;
